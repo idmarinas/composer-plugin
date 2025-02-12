aa47aad01c908b111b9962245d0febad4ebadd39
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-12 15:39:55 +0100

    feat: add DefaultBranchTrait for handling default branch input
    - Introduced a new trait `DefaultBranchTrait` to manage user input
      for the default branch name in a Symfony console command.
    - The trait utilizes Symfony's validation constraints to ensure
      that the input is not blank and contains no suspicious characters.

---
+++ src/Traits/Command/DefaultBranchTrait.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 12/02/2025, 15:32
+ *
+ * @project IDMarinas Composer Plugin
+ * @see     https://github.com/idmarinas/composer-plugin
+ *
+ * @file    DefaultBranchTrait.php
+ * @date    12/02/2025
+ * @time    15:32
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Composer\Plugin\Traits\Command;
+
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Validator\Constraints\NoSuspiciousCharacters;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Validation;
+
+trait DefaultBranchTrait
+{
+	/** @internal */
+	private function defaultBranch (SymfonyStyle $io): string
+	{
+		$validation = Validation::createCallable(
+			new NotBlank(allowNull: false),
+			new NoSuspiciousCharacters(),
+		);
+
+		return $io->ask('Default branch name of repository" to', 'master', $validation);
+	}
+}
