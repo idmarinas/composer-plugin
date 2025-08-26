3987bb48e12ba8b30a42a5cd2661ff407ae205fc
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-26 18:54:17 +0200

    feat(AppInfo): add new AppInfo class extending AbstractInfo
    - Introduced `AppInfo` class to manage application-specific repository information.
    - Implemented methods for project name retrieval and branch designation.

---
+++ src/AppInfo.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 26/08/2025, 13:46
+ *
+ * @project IDMarinas Composer Plugin
+ * @see     https://github.com/idmarinas/composer-plugin
+ *
+ * @file    AppInfo.php
+ * @date    26/08/2025
+ * @time    13:46
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.4.0
+ */
+
+namespace Idm\Composer\Plugin;
+
+final class AppInfo extends AbstractInfo
+{
+	public function __construct (string $repository)
+	{
+		$this->setRepository($repository);
+	}
+
+	public function getProjectName (string $name = ''): string
+	{
+		return parent::getProjectName($this->repositoryName);
+	}
+
+	public function getBranch (): string
+	{
+		return '1.x';
+	}
+
+}
