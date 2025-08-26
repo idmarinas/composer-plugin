6e7f4ea51f3b8aec954f7796851efc3eaf6755b5
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-26 19:16:52 +0200

    fix(CustomizeIdmAppCommand): add `.deployer` directory to exclusion list in Finder

--- src/Command/CustomizeIdmAppCommand.php
+++ src/Command/CustomizeIdmAppCommand.php
@@ -101,7 +101,7 @@ EOF
 			->ignoreDotFiles(false)
 			->ignoreVCSIgnored(true)
 			->ignoreUnreadableDirs()
-			->exclude(['bundles', 'docs', '.docker'])
+			->exclude(['bundles', 'docs', '.docker', '.deployer'])
 			->notName(['.editorconfig', '.gitkeep'])
 			->files()
 			->sortByName()
