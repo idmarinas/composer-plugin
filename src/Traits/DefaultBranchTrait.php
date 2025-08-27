0735c015b9e0aa5243dd0239c5f053436ad116a6
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-08-27 12:25:29 +0200

    fix(DefaultBranchTrait): correct typo in default branch prompt text

--- src/Traits/DefaultBranchTrait.php
+++ src/Traits/DefaultBranchTrait.php
@@ -33,6 +33,6 @@ trait DefaultBranchTrait
 			new NoSuspiciousCharacters(),
 		);
 
-		return self::io()->ask('Default branch name of repository" to', '1.x', $validation);
+		return self::io()->ask('Default branch name of repository to', '1.x', $validation);
 	}
 }
